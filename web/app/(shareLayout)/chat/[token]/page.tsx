16e41aa110d099f418a4a2c3290d5a20b770fb98
zhangjq12 <524796875@qq.com>
2025-04-17 16:34:03 +0800

    sxu-fix: 更改chat登录时机（未测试）2

--- web/app/(shareLayout)/chat/[token]/page.tsx
+++ web/app/(shareLayout)/chat/[token]/page.tsx
@@ -1,40 +1,8 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import ChatWithHistoryWrap from '@/app/components/base/chat/chat-with-history'
-import { setIsIframe } from '@/utils/globalIsIframe'
-import { login } from '@/service/common'
 
 const Chat = () => {
-  const handleIframeLogin = async (e: any) => {
-    const data = e.data
-    const email = data.email
-    const password = data.password
-    const loginData: Record<string, any> = {
-      email,
-      password,
-      language: 'zh-Hans',
-      remember_me: true,
-    }
-
-    setIsIframe(true)
-    const loginProcess = async () => {
-      const res = await login({
-        url: '/signuplogin',
-        body: loginData,
-      })
-      if (res.result === 'success') {
-        localStorage.setItem('console_token', res.data.access_token)
-        localStorage.setItem('refresh_token', res.data.refresh_token)
-      }
-    }
-    await loginProcess()
-  }
-
-  useEffect(() => {
-    if (typeof window !== 'undefined')
-      window.onmessage = handleIframeLogin
-  }, [])
-
   return (
     <ChatWithHistoryWrap />
   )
