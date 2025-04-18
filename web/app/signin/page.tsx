26a7ef97edc872839557140ae85b4fb11abf5a4b
zhangjq12 <524796875@qq.com>
2025-04-18 11:36:36 +0800

    sxu-fix: 新建signuplogin页面尝试做登录

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -2,39 +2,10 @@
 import { useSearchParams } from 'next/navigation'
 import OneMoreStep from './oneMoreStep'
 import NormalForm from './normalForm'
-import { useIsIframe } from '@/context/isIframe-context'
 
 const SignIn = () => {
   const searchParams = useSearchParams()
   const step = searchParams.get('step')
-  const [isIframe] = useIsIframe()
-
-  // useEffect(() => {
-  //   const loginProcess = async () => {
-  //     console.log(isIframe)
-  //     const loginData = localStorage.getItem('loginData') || ''
-  //     const data = JSON.parse(loginData)
-  //     data.language = 'zh-Hans'
-  //     const res = await login({
-  //       url: '/signuplogin',
-  //       body: data,
-  //     })
-  //     if (res.result === 'success') {
-  //       localStorage.setItem('console_token', res.data.access_token)
-  //       localStorage.setItem('refresh_token', res.data.refresh_token)
-  //       // window.location.replace('/apps')
-  //       const redirectUrl = localStorage.getItem('redirect_url')
-  //       if (redirectUrl) {
-  //         localStorage.removeItem('redirect_url')
-  //         router.replace(redirectUrl)
-  //       } else {
-  //         router.replace('/apps')
-  //       }
-  //     }
-  //   }
-  //   // console.log(isIframe)
-  //   loginProcess()
-  // }, [isIframe])
 
   if (step === 'next')
     return <OneMoreStep />
---
+++ web/app/signupLogin/page.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { login } from '@/service/common'
+import { setIsIframe } from '@/utils/globalIsIframe'
+import { useEffect, useState } from 'react'
+
+const SignUpLogin = () => {
+  const [loginData, setLoginData] = useState<Record<string, any> | undefined>()
+
+  useEffect(() => {
+    setIsIframe(true)
+    const handleLoginData = (e: any) => {
+      console.log(e.data)
+      if (e.data.env === 'developer') {
+        const data = e.data
+        const email = data.email
+        const password = data.password
+        const loginData: Record<string, any> = {
+          email,
+          password,
+          language: 'zh-Hans',
+          remember_me: true,
+        }
+
+        setLoginData(loginData)
+      }
+    }
+
+    const process = async () => {
+      if (loginData) {
+        const res = await login({
+          url: '/signuplogin',
+          body: loginData,
+        })
+        if (res.result === 'success') {
+          localStorage.setItem('console_token', res.data.access_token)
+          localStorage.setItem('refresh_token', res.data.refresh_token)
+        }
+        parent.window.postMessage({ finish: true }, '*')
+      }
+    }
+    process()
+
+    window.addEventListener('message', handleLoginData, false)
+
+    return () => {
+      window.removeEventListener('message', handleLoginData, false)
+    }
+  }, [])
+
+  return <div></div>
+}
+
+export default SignUpLogin
