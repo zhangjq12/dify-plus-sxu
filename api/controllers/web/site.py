af46685fbc703b953fdffcc8aed8885d5620ee0a
OpenHuShen <hushen@alu.sxu.edu.cn>
2025-05-19 10:01:57 +0800

    fix:屏蔽部分web site api返回信息

--- api/controllers/web/site.py
+++ api/controllers/web/site.py
@@ -44,7 +44,7 @@ class AppSiteApi(WebApiResource):
 
     app_fields = {
         # "app_id": fields.String,
-        # "end_user_id": fields.String,
+        "end_user_id": fields.String,
         "enable_site": fields.Boolean,
         "site": fields.Nested(site_fields),
         "model_config": fields.Nested(model_config_fields, allow_null=True),
